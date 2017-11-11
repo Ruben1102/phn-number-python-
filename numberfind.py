c7a936ed45ff8929243325e670ebacc279465efb
Ruben classle <rubenraj@classle.co.in>
2017-11-11 14:20:02 +0530

    improved code reduced steps && instruction added

--- numberfind.py
+++ numberfind.py
@@ -10,80 +10,87 @@ five_digit = range (0, 100000)
 #six_digit = range (100000, 999999)
 #seven_digit = range (1000000, 9999999)
 
-argv = sys.argv[1:]
-opts, args = getopt.getopt(argv, "p:", ["n="]) #args #shortopts #longopts
-#print(opts)
+def usage():
+	print("\nUsage: options are\n")
+	print("      -p: Enter the number you barely remember")
+	print("      -n: give the name to search (Optional)\n")
 
-for opt, arg in opts:
-	if opt == '-p':
-		number = arg
-	elif opt == '--n':
-		name = arg
-#print(type(number)) 
-# print(args) 
-
-num = []
-nums = []
-# num.append(number)
-no = 10 - len(number)
- 
-while no:
-	print ("\n[+] Geanerating sequences of number:")
-	print ("------------------------------------\n")
-	if no == 4:
-		for n in four_digit:
-			nums.append(number + str(n).zfill(no))
-			#print(number + str(n).zfill(no))
-		break
-	elif no ==3:
-		for n in three_digit:
-			nums.append(number + str(n).zfill(no))
-			#print (number + str(n).zfill(no))
-		break
-	elif no == 2:
-		for n in two_digit:
-			nums.append(number + str(n).zfill(no))
-			#print(number + str(n).zfill(no))
-		break
-	elif no == 1:
-		for n in one_digit:
-			nums.append(number + str(n).zfill(no))
-			#print(nums)
-		break
+def start(v):
+	argv = sys.argv[1:]
+	try:
+		opts, args = getopt.getopt(argv, "p:n") #args #shortopts #longopts
+	except getopt.GetoptError:
+		usage()
+	#print(opts)
+	num_search = False
+	for opt, arg in opts:
+		if opt == '-p':
+			number = arg
+		elif opt == '-n':
+			name = arg
+			num_search = True
+	nums = []
+	no = 10 - len(number)
+	 
+	while no:
+		print ("\n[+] Geanerating sequences of number:")
+		print ("------------------------------------\n")
+		if no == 4:
+			for n in four_digit:
+				nums.append(number + str(n).zfill(no))
+				#print(number + str(n).zfill(no))
+			break
+		elif no ==3:
+			for n in three_digit:
+				nums.append(number + str(n).zfill(no))
+				#print (number + str(n).zfill(no))
+			break
+		elif no == 2:
+			for n in two_digit:
+				nums.append(number + str(n).zfill(no))
+				#print(number + str(n).zfill(no))
+			break
+		elif no == 1:
+			for n in one_digit:
+				nums.append(number + str(n).zfill(no))
+				#print(nums)
+			break
+		else:
+			print('number not valid')
+			break #Choosing sequence based on length of given number
 	else:
-		print('number not valid')
-		break
-else:
-	print('\n', 'number should be less than 10 and grater than 5', '\n')
+		print('\n', 'number should be less than 10 and grater than 5', '\n')
 
-if nums:
-	print ("\n[+] Writing number in file:")
-	print ("------------------------------------\n")
-	file_name = "numbers.txt"
-	f = open(file_name, "w")
-	header = "numbers found:\n"
-	f.write(header)
-	for s in nums:
-		#td = sc.b
-		#tw = sc.a["href"]
-		f.write(" -" + s + "\n")
-	print(' writed ')
-	f.close()
-for nomber in nums: #Checking a combination from the list of all possible combinations
-	url = 'http://numberfinder.com/find.php?q=' + nomber + "&submit=ok" #Making a request to the phone lookup web service
-	result = urlopen(url).read().lower() #Reading the response
-
-	if 'googlebot' in str(result): #Checking if neha is in the response
-		# print (nomber) #Prints the combination whose result has neha in it
-		file_name = "web.txt"
-		f = open(file_name, "a")
-		header = "web found:\n"
+	if nums:
+		print ("\n[+] Writing number in file:")
+		print ("------------------------------------\n")
+		file_name = "numbers.txt"
+		f = open(file_name, "w")
+		header = "numbers found:\n"
 		f.write(header)
-		f.write(" -" + nomber + '\n')
+		for s in nums:
+			#td = sc.b
+			#tw = sc.a["href"]
+			f.write(" -" + s + "\n")
+		print(' writed ')
 		f.close()
-	else:
-		pass
-		#print ("no", nomber)
+	
+	if num_search:
+		for nomber in nums: #Checking a combination from the list of all possible combinations
+			url = 'http://numberfinder.com/find.php?q=' + nomber + "&submit=ok" #Making a request to the phone lookup web service
+			result = urlopen(url).read().lower() #Reading the response
+
+			if 'nesa' in str(result): #Checking if neha is in the response
+				# print (nomber) #Prints the combination whose result has neha in it
+				file_name = "web.txt"
+				f = open(file_name, "a")
+				header = "web found:\n"
+				f.write(header)
+				f.write(" -" + nomber + '\n')
+				f.close()
+			else:
+				pass
+				print ("name cann't be found")
 
 """
 for n in range(no, 0, -1):
@@ -95,23 +102,6 @@ for n in range(no, 0, -1):
 for i in num:
 	print(i)
 """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 """ Old Version got Updated on 11/11/17
 # Checking for 1 digit combination
@@ -150,3 +140,12 @@ for number in one_digit: #Checking a combination from the list of all possible c
 	else:
 		pass
 """
+
+if __name__ == "__main__":
+	try:
+		start(sys.argv[1:])
+	except KeyboardInterrupt:
+		print ("Search interrupted by user..")
+	except:
+		usage()
+		sys.exit()
